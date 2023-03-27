3a157ce4b3ea94bc434413d17fe1734d62aa18c6
sdfpalta75 <sdfmail@gmail.com>
2023-03-27 00:31:05 -0300

    Creación Proyecto ProDe

---
+++ ProDe/src/main/java/ProDe/ProDe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ProDe;
+
+/**
+ *
+ * @author Seba
+ */
+public class ProDe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
